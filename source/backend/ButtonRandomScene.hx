832b764cad4d7a370ad12f337b30008afb0bf123
Viwu <sinconsistencia@gmail.com>
2025-12-25 03:40:19 -0600

    several tweaks to button random stuff

--- source/backend/ButtonRandomScene.hx
+++ source/backend/ButtonRandomScene.hx
@@ -11,7 +11,6 @@ class ButtonRandomScene extends ButtonScene
 	{
 		super(instruction, laststate, buttonSize);
 		this.buttonScreenCenter = false;
-		this.clickText.visible = false;
 
 		this.padding = padding;
 		this.buttonScaleOffset = buttonScaleOffset;
@@ -21,9 +20,37 @@ class ButtonRandomScene extends ButtonScene
 	{
 		super.create();
 
+		button.alpha = .5;
 		button.scale.set(1 - buttonScaleOffset, 1 - buttonScaleOffset);
-        button.updateHitbox();
-        
+		button.updateHitbox();
+
+		randomizeButtonPosition();
+
+		this.onLeftClick.add(function()
+		{
+			randomizeButtonPosition();
+		});
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		this.clickText.screenCenter();
+
+		if (!transitioning && FlxG.mouse.overlaps(button))
+		{
+			button.scale.set(0.9 - buttonScaleOffset, 0.9 - buttonScaleOffset);
+
+			if (FlxG.mouse.pressed)
+				button.scale.set(0.8 - buttonScaleOffset, 0.8 - buttonScaleOffset);
+		}
+		else
+			button.scale.set(1 - buttonScaleOffset, 1 - buttonScaleOffset);
+	}
+
+	public function randomizeButtonPosition()
+	{
 		button.setPosition(FlxG.random.float(padding, (FlxG.width - button.width) - padding),
 			FlxG.random.float(padding, (FlxG.height - button.height) - padding));
 	}
