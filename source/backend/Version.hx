6f4ad09cfcc69fc9045bab3d5ccd8ae9bc57d7dd
Viwu <sinconsistencia@gmail.com>
2025-12-25 00:36:30 -0600

    version class

---
+++ source/backend/Version.hx
@@ -0,0 +1,37 @@
+package backend;
+
+import flixel.FlxG;
+
+class Version
+{
+	public static var MAJOR(get, never):Int;
+
+	static function get_MAJOR():Int
+	{
+		return 1;
+	}
+	public static var MINOR(get, never):Int;
+
+	static function get_MINOR():Int
+	{
+		return 0;
+	}
+
+	public static var BUILD(get, never):Int;
+
+	static function get_BUILD():Int
+	{
+        if (FlxG.save.isBound)
+            if (FlxG.save.data.build != null)
+                return cast FlxG.save.data.build;
+
+		return 0;
+	}
+
+	public static var FULL(get, never):String;
+
+	static function get_FULL():String
+	{
+		return [MAJOR, MINOR, BUILD].join('.');
+	}
+}
