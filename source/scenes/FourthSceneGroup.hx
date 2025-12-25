8cad7b84ab053b24b536b6288c10fc8ec3e0bafd
Viwu <sinconsistencia@gmail.com>
2025-12-25 10:06:15 -0600

    wait time adjustments

--- source/scenes/FourthSceneGroup.hx
+++ source/scenes/FourthSceneGroup.hx
@@ -8,6 +8,8 @@ class Scene11Normal extends ButtonAppearingAmountScene
 	{
 		super(5, 'Soon a number will appear, click the box that amount', 'scene11n');
 
+		waitMax = 4.0;
+
 		onCompletion.add(function()
 		{
 			switchScene(new Scene12Normal());
@@ -20,6 +22,8 @@ class Scene11Impatient extends ButtonAppearingAmountScene
 	override public function new()
 	{
 		super(4, 'Soon a number will appear, click the box that amount', 'scene11i');
+		
+		waitMax = 3.0;
 
 		onCompletion.add(function()
 		{
@@ -34,6 +38,8 @@ class Scene12Normal extends ButtonAppearingAmountScene
 	{
 		super(8, 'Repeat', 'scene12n');
 
+		waitMax = 8.0;
+
 		onCompletion.add(function()
 		{
 			switchScene(new Scene13Normal());
@@ -46,6 +52,8 @@ class Scene12Impatient extends ButtonAppearingAmountScene
 	override public function new()
 	{
 		super(7, 'Repeat', 'scene12i');
+		
+		waitMax = 6.0;
 
 		onCompletion.add(function()
 		{
@@ -60,6 +68,8 @@ class Scene13Normal extends ButtonAppearingAmountScene
 	{
 		super(15, '', 'scene13n');
 
+		waitMax = 12.0;
+
 		onCompletion.add(function()
 		{
 			switchScene(new Scene14Normal());
@@ -72,6 +82,8 @@ class Scene13Impatient extends ButtonAppearingAmountScene
 	override public function new()
 	{
 		super(10, '', 'scene13i');
+		
+		waitMax = 10.0;
 
 		onCompletion.add(function()
 		{
@@ -86,6 +98,8 @@ class Scene14Normal extends ButtonAppearingAmountScene
 	{
 		super(20, '', 'scene14n');
 
+		waitMax = 16.0;
+
 		onCompletion.add(function()
 		{
 			switchScene(new EndNormalV1());
@@ -98,6 +112,8 @@ class Scene14Impatient extends ButtonAppearingAmountScene
 	override public function new()
 	{
 		super(15, '', 'scene14i');
+		
+		waitMax = 15.0;
 
 		onCompletion.add(function()
 		{
