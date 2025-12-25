ade86d1c02a8f501f2a6642e48e11166f0a3d9b2
Viwu <sinconsistencia@gmail.com>
2025-12-24 19:21:56 -0600

    init state + save data build

---
+++ source/InitState.hx
@@ -0,0 +1,23 @@
+package;
+
+import lime.app.Application;
+import flixel.FlxG;
+import flixel.FlxState;
+
+class InitState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+
+		FlxG.save.bind('tepro', Application.current.meta.get('company'));
+
+		#if debug
+		if (FlxG.save.data.build == null)
+			FlxG.save.data.build = 0;
+		
+		FlxG.save.data.build++;
+		trace(FlxG.save.data.build);
+		#end
+	}
+}
