8fa2eba8975fcf9f5c280b77bb0b51eefb18b310
Viwu <sinconsistencia@gmail.com>
2025-12-26 01:00:24 -0600

    confirm

--- source/subscenes/ResetSaveSubstate.hx
+++ source/subscenes/ResetSaveSubstate.hx
@@ -1,17 +1,35 @@
 package subscenes;
 
+import flixel.FlxG;
 import flixel.text.FlxText;
 import backend.Subscene;
 
 class ResetSaveSubstate extends Subscene
 {
-	public var omniMan:FlxText = new FlxText(0, 0, 0, "Are you sure?", 16);
+	public var omniMan:FlxText = new FlxText(0, 0, 0, "Are you sure?\n(Y/N)", 16);
 
 	override function create()
 	{
 		super.create();
 
-        omniMan.screenCenter();
-        add(omniMan);
+		omniMan.screenCenter();
+		add(omniMan);
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justReleased.Y)
+		{
+			FlxG.save.erase();
+			FlxG.resetGame();
+			close();
+		}
+
+		if (FlxG.keys.justReleased.N)
+		{
+			close();
+		}
 	}
 }
